280675d107d26f072c1fe16b9b3351accee7203f
scherzer <scherzer@hs-weingarten.de>
2018-11-02 18:38:27 +0100

    quick commit

--- src/ShaderForm/Demo/UniformHelper.cs
+++ src/ShaderForm/Demo/UniformHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShaderForm.Demo
+﻿namespace ShaderForm.Demo
 {
 	public static class UniformHelper
 	{
